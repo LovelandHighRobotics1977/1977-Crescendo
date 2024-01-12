4dabdea71c4f4d9aa01dae67d4685f2ca3e9417a
ThatAlo <52977682+ThatAlo@users.noreply.github.com>
2024-01-11 17:48:36 -0700

    Delete src/main/cpp/subsystems/ArmComponents directory

--- src/main/cpp/subsystems/ArmComponents/Intake.cpp
+++
@@ -1,9 +0,0 @@
-#include "subsystems/ArmComponents/Intake.h"
-
-Intake::Intake() :m_intakeMotor{Mechanism::Intake::ID}{
-	m_intakeMotor.SetNeutralMode(Brake);
-}
-
-void Intake::setIntake(double speed){
-	m_intakeMotor.Set(speed);
-}
