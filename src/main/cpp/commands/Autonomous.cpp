102eecef83d7a8afdcb43648d0c1d5ce869cb4c3
ThatAlo <52977682+ThatAlo@users.noreply.github.com>
2024-01-25 08:56:05 -0700

    Delete src directory

--- src/main/cpp/commands/Autonomous.cpp
+++
@@ -1,7 +0,0 @@
-#include "commands/Autonomous.h"
-
-frc2::CommandPtr AutoRoutine::test(DriveSubsystem *drive) {
-	return frc2::SequentialCommandGroup(
-		drive->ZeroOdometry({0_m, 0_m, 0_deg})
-	).ToPtr();
-}
