55692a869dfbc2d3c13774524d2c147f61e35569
ThatAlo <52977682+ThatAlo@users.noreply.github.com>
2024-02-01 19:05:42 -0700

    Added license and credits to Headers Directory

--- src/main/include/Headers/Headers.h
+++ src/main/include/Headers/Headers.h
@@ -1,11 +1,8 @@
 #ifndef _HEADERS_H
 #define _HEADERS_H
 
-/**
- * Copyright (c) FIRST and other WPILib contributors. 
- * Open Source Software; you can modify and/or share it under the terms of 
- * the WPILib BSD license file in the root directory of this project.
-*/
+// Project Info, contains credits, license, and other important stuffs
+#include "ProjectInfo.h"
 
 // Libraries ( very important )
 #include "Libraries.h"
---
+++ src/main/include/Headers/Projectinfo.h
@@ -0,0 +1,69 @@
+/*
+ __         __     ______     ______     __   __     ______     ______    
+/\ \       /\ \   /\  ___\   /\  ___\   /\ "-.\ \   /\  ___\   /\  ___\   
+\ \ \____  \ \ \  \ \ \____  \ \  __\   \ \ \-.  \  \ \___  \  \ \  __\   
+ \ \_____\  \ \_\  \ \_____\  \ \_____\  \ \_\\"\_\  \/\_____\  \ \_____\ 
+  \/_____/   \/_/   \/_____/   \/_____/   \/_/ \/_/   \/_____/   \/_____/ 
+                                                                          
+Copyright (c) FIRST and other WPILib contributors. 
+Open Source Software; you can modify and/or share it under the terms of 
+the WPILib BSD license file in the root directory of this project.
+
+The license is also pasted below for reference:
+
+================
+
+Copyright (c) 2009-2024 FIRST and other WPILib contributors
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+   * Redistributions of source code must retain the above copyright
+     notice, this list of conditions and the following disclaimer.
+   * Redistributions in binary form must reproduce the above copyright
+     notice, this list of conditions and the following disclaimer in the
+     documentation and/or other materials provided with the distribution.
+   * Neither the name of FIRST, WPILib, nor the names of other WPILib
+     contributors may be used to endorse or promote products derived from
+     this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY FIRST AND OTHER WPILIB CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY NONINFRINGEMENT AND FITNESS FOR A PARTICULAR
+PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL FIRST OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+================
+ ______     ______     ______     _____     __     ______   ______    
+/\  ___\   /\  == \   /\  ___\   /\  __-.  /\ \   /\__  _\ /\  ___\   
+\ \ \____  \ \  __<   \ \  __\   \ \ \/\ \ \ \ \  \/_/\ \/ \ \___  \  
+ \ \_____\  \ \_\ \_\  \ \_____\  \ \____-  \ \_\    \ \_\  \/\_____\ 
+  \/_____/   \/_/ /_/   \/_____/   \/____/   \/_/     \/_/   \/_____/ 
+                                                                      
+Lead Programmer: 
+    - Salem Lessem
+      https://github.com/ThatAlo
+
+Assistant Programmer: 
+    - Dean Morgan
+      https://github.com/Dean27817
+
+Loveland High Robotics programming team:
+    [ Wyllow  ]      [ Dinesh ]
+    [ Collin  ]      [ Jacob  ]
+    [ Bradley ]      [ Willem ]
+
+Team mentors:
+    - Mike Pintaric
+    - Bruce Bowman
+
+FRC teams:
+    - 1977: The PowerSquids
+    - 1822: Electric Calamari
+
+*/
