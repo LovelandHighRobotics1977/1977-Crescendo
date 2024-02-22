15873672f7f56c72b56944c864b62ed9e0498d4a
ThatAlo <52977682+ThatAlo@users.noreply.github.com>
2024-02-22 10:10:39 -0700

    Delete src directory

--- src/main/include/RobotBase/Parameters/RobotParameters.h
+++
@@ -1,8 +0,0 @@
-#ifndef _ROBOT_PARAMETERS_H
-#define _ROBOT_PARAMETERS_H
-
-#include "AutoParameters.h"
-#include "TeleopParameters.h"
-#include "MechParameters.h"
-
-#endif
--- src/main/include/Subsystems/Devices/Devices.h
+++
@@ -1,8 +0,0 @@
-#ifndef _AUX_DEVICES_H
-#define _AUX_DEVICES_H
-
-#include "Field.h"
-#include "Gyro.h"
-#include "PDH.h"
-
-#endif	// _AUX_DEVICES_H
